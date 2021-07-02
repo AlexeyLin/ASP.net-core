485fc27c4f0651b8ff3470399b0d73756a90b605
AlexeyLin <aleksey.lin@gmail.com>
2021-07-02 18:30:41 +0300

    Update unit test

--- MetricsManger/MetricsAgentTests/HddControllerUnitTests.cs
+++ MetricsManger/MetricsAgentTests/HddControllerUnitTests.cs
@@ -1,32 +1,48 @@
 ﻿using MetricsAgent.Controllers;
-using MetricsAgent;
 using System;
 using Xunit;
-using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MetricsAgent.DAL.Interfaces;
+using MetricsAgent.DAL.Models;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace MetricsAgentTests
 {
     public class HddControllerUnitTests
     {
-        private HddMetricsController controller;
+        private readonly HddMetricsController _controller;
+        private readonly Mock<IHddMetricsRepository> _mock;
+        private readonly Mock<ILogger<HddMetricsController>> _mockLog;
 
         public HddControllerUnitTests()
         {
-            controller = new HddMetricsController();
+            _mock = new Mock<IHddMetricsRepository>();
+            _mockLog = new Mock<ILogger<HddMetricsController>>();
+            _controller = new HddMetricsController(_mockLog.Object, _mock.Object);
         }
 
         [Fact]
-        public void GetLeftCapacity_ReturnsOk()
+        public void Create_ReturnsOk()
         {
-            //Arrange
-            var fromTime = new DateTimeOffset();
-            var toTime = new DateTimeOffset();
+            _mock.Setup(repository => repository.Create(It.IsAny<HddMetric>())).Verifiable();
 
-            //Act
-            var result = controller.GetLeftCapacity(fromTime, toTime);
+            var result = _controller.Create(new MetricsAgent.Requests.HddMetricCreateRequest { Time = DateTimeOffset.FromUnixTimeSeconds(0), Value = 50 });
 
-            // Assert
-            _ = Assert.IsAssignableFrom<IActionResult>(result);
+            _mock.Verify(repository => repository.Create(It.IsAny<HddMetric>()), Times.Once());
+        }
+
+        [Fact]
+        public void GetMetrics_ReturnsOk()
+        {
+            var fromTime = DateTimeOffset.FromUnixTimeSeconds(0);
+            var toTime = DateTimeOffset.FromUnixTimeSeconds(100);
+
+            _mock.Setup(repository => repository.GetByTimePeriod(fromTime, toTime)).Returns(new List<HddMetric>());
+
+            var result = _controller.GetLeftCapacity(fromTime, toTime);
+
+            _mock.Verify(repository => repository.GetByTimePeriod(fromTime, toTime), Times.Once());
         }
     }
 }
